a5cfbf43159e1d869d0ad31283993b4c26242f30
Marcel Stimberg <marcel.stimberg@inserm.fr>
2021-11-26 14:08:09 +0100

    Remove "Source" links on top of page
    This are rarely useful and can confuse search engines, etc.
    All sources are in the public github repository anyway

--- conf.py
+++ conf.py
@@ -1436,10 +1436,10 @@ MARKDOWN_EXTENSIONS = ['markdown.extensions.fenced_code', 'markdown.extensions.c
 # """
 
 # Show link to source for the posts?
-# SHOW_SOURCELINK = True
+SHOW_SOURCELINK = False
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
-# COPY_SOURCES = True
+COPY_SOURCES = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
